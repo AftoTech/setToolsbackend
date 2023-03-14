bfc80288fa88b05419fa05954facd1af89257c4b
AftoTech <afto.tech2021@gmail.com>
2023-03-14 08:30:26 +0530

    keywordextractor

---
+++ constant.js
@@ -0,0 +1,8 @@
+const stopWords = [
+    'for', 'the', 'a', 'stands', 'test', 'man', 'woman',
+    
+]
+
+module.exports = {
+    stopWords: stopWords
+};
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const { parser } = require("html-metadata-parser");
-
+const { stopWords } = require("./constant");
+const NodeRakeV2 = require('node-rake-v2').NodeRakeV2;
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -13,13 +14,31 @@ app.post("/metatagextract", (req, res) => {
     if (!inputData) throw new Error("No data provided");
     // Do some processing with the input data
     parser(inputData?.url).then((result) => {
-      res.json(JSON.stringify(result, null, 3));
+      res.json(result, null, 3);
     });
   } catch (error) {
     console.error(error.message);
     res.status(400).json({ error: error.message });
   }
 });
+
+app.post("/keywordExtractor", (req, res) => {
+
+  const node_rake_v2 = new NodeRakeV2();
+  node_rake_v2.addStopWords(stopWords)
+
+    try {
+    const inputData = req.body;
+      if (!inputData) throw new Error("No data provided");
+        const keywords = node_rake_v2.generate(inputData.text);
+      res.json(keywords)
+    // // Do some processing with the input data
+    
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ error: error.message });
+  }
+})
 
 app.get("/message", (req, res) => {
   res.json({ message: "Hello from server!" });
